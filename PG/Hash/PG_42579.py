3bff47528ebd1fea83ef4160dfba9656922f02d3
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-09 00:01:26 +0900

    Solve : PG 42579 해결

---
+++ PG/Hash/PG_42579.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+def solution(genres, plays):
+    answer=[]
+    genres_count=defaultdict(int)
+    genres_idx=defaultdict(list)
+
+    for i in range(len(genres)) :
+        genres_count[genres[i]]+=plays[i]
+        genres_idx[genres[i]].append([plays[i], i])
+
+    genres_count=sorted(genres_count, key=lambda x:-genres_count[x])
+
+    for genre in genres_count :
+        genre_list=genres_idx[genre]
+        genre_list.sort(key=lambda x:(-x[0], x[1]))
+        count=0
+
+        for play, idx in genre_list :
+            count+=1
+            if count>2 :
+                break
+            answer.append(idx)
+
+    return answer
+
+print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
