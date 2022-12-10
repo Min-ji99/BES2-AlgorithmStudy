60243c3ebaf8c0491753264abc94d230d8ee7060
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-10 21:50:56 +0900

    Solve : PG 42862 해결

---
+++ PG/Greedy/PG_42862.py
@@ -0,0 +1,13 @@
+def solution(n, lost, reserve):
+    new_lost=set(lost)-set(reserve)
+    new_reserve=set(reserve)-set(lost)
+
+    for student in new_reserve :
+        if student-1 in new_lost:
+            new_lost.remove(student-1)
+        elif student+1 in new_lost :
+            new_lost.remove(student+1)
+    return n-len(new_lost)
+
+print(solution(5, [2, 4], [1, 3, 5]))
+print(solution(5, [2, 4], [3]))
